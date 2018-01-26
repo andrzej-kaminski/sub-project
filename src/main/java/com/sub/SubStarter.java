d5b3030285ed40554666975e5dd6c7f94fcee1d9
andrzej-kaminski <andrzej.st.kaminski@gmail.com>
2018-01-26 10:56:02 +0100

    remove stop

--- src/main/java/com/sub/SubStarter.java
+++ src/main/java/com/sub/SubStarter.java
@@ -4,5 +4,8 @@ public class SubStarter {
     public void subStart() {
         System.out.println("LOG-Sub Module Started");
     }
+    public void stop() {
+        System.out.println("Stop");
+    }
 
 }
